77ecd0439afcafa3639ad715605fd46f9713a766
Matthias Seghers <19349429+matthiasseghers@users.noreply.github.com>
2023-10-15 21:08:00 +0200

    Update install.sh

--- git/install.sh
+++ git/install.sh
@@ -12,14 +12,12 @@ ln -sf $HOME/.dotfiles/git/.global-gitignore $HOME/.gitignore
 echo "\033[0;32mDone"
 
 # Setup Git
-if [[ ! -f $HOME/.gitignore ]]; then
-	echo "Configure Git..."
-	read -p 'Git Username: ' GIT_USERNAME
-	read -p 'Git email: ' GIT_EMAIL
-	git config --global user.name "$GIT_USERNAME" # add github account
-	git config --global user.email $GIT_EMAIL     # add github email
-	git config --global core.excludesFile $HOME/.gitignore
-        echo "\033[0;32mDone"
-fi
+echo "Configure Git..."
+read -p 'Git Username: ' GIT_USERNAME
+read -p 'Git email: ' GIT_EMAIL
+git config --global user.name "$GIT_USERNAME" # add github account
+git config --global user.email $GIT_EMAIL     # add github email
+git config --global core.excludesFile $HOME/.gitignore
+echo "\033[0;32mDone"
 
 exit 0
